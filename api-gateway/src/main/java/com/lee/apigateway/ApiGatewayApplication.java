bbb9c289f4df385f50d84d2cee8db1b06d45483f
lizhilin <1345273527@qq.com>
2021-09-14 18:12:19 +0800

    no message

--- api-gateway/src/main/java/com/lee/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/lee/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,23 @@
 package com.lee.apigateway;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public IRule loadBalanceRule(){
+        return  new NacosRule();
     }
 
 }
